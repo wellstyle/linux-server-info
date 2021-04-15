32e7218cd28cd3e58aa36f2bccc4926192b745a7
Chris Edwards <>
2021-04-15 15:01:10 -0500

    when kB is read for the memory it is converted to G

--- server-info.sh
+++ server-info.sh
@@ -11,7 +11,8 @@ CPU=`cat /proc/cpuinfo | grep "model name" | head -1 | awk -F':' '{print $2}' | 
 CPU_NUM=`cat /proc/cpuinfo | grep -c processor`
 CPU_MHZ=`cat /proc/cpuinfo | grep "cpu MHz" | head -1 | awk -F':' '{print $2}' | cut -c2-`
 M_TYPE=`uname -m`
-MEM_SIZE=`cat /proc/meminfo | grep MemTotal | awk '{print $2,$3}'`
+MEM_SIZE=`cat /proc/meminfo | grep MemTotal | awk '{print $2}'`
+MEM_SIZE_KB=`cat /proc/meminfo | grep MemTotal | awk '{print $3}'`
 HDD_SIZE=`lsblk --nodeps --noheadings --output NAME,SIZE`
 LOCALE=`echo $LANG`
 
@@ -23,7 +24,12 @@ printInfo "CPU " "$CPU"
 printInfo "CPU Number" "$CPU_NUM"
 printInfo "CPU MHz" "$CPU_MHZ"
 printInfo "Machine Type" "$M_TYPE"
-printInfo "Memory Size" "$MEM_SIZE"
+if [ "$MEM_SIZE_KB" = "kB" ]
+then
+  MEM_SIZE=`echo "scale=2; $MEM_SIZE/1000000" | bc`
+  MEM_SIZE_KB="G"
+fi
+printInfo "Memory Size" "$MEM_SIZE$MEM_SIZE_KB"
 for hdd in $HDD_SIZE; do
         printInfo "HDD Size" "$hdd"
 done
